596f122a7c9579b0f48101b09db7365d69b0298c
Germano <germanno.verde@gmail.com>
2023-11-18 17:17:22 -0300

    configurando frontend

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,4 @@
-/*const express = require('express');
-const app = express();
-const db = require('./db');
-require('dotenv').config();
-
-const port = process.env.PORT || 3000;
-
-const pictureRouter = require('./routes/picture');
-
-app.use('/pictures', pictureRouter);
-
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
-*/
+/*
 
 const express = require('express');
 const cors = require('cors');
@@ -32,5 +18,26 @@ app.use('/pictures', pictureRouter);
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
+*/
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+require('dotenv').config();
+require('./db');
+
+const port = process.env.PORT || 3000;
+
+app.use(cors()); // Middleware CORS
+app.use(express.json()); // Middleware para analisar o corpo das solicitações como JSON
+
+const pictureRouter = require('./routes/picture');
+app.use('/pictures', pictureRouter);
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
+
+
 
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,69 +1,78 @@
+
 document.addEventListener('DOMContentLoaded', () => {
-    const uploadForm = document.getElementById('uploadForm');
-    const imageList = document.getElementById('imageList');
-  
-    // Função para carregar imagens
-    async function loadImages() {
-      const response = await fetch('/pictures/images');
-      const data = await response.json();
-  
+  const uploadForm = document.getElementById('uploadForm');
+  const imageList = document.getElementById('imageList');
+
+  // Função para carregar imagens
+  async function loadImages() {
+    try {
+      const response = await fetch('http://localhost:3000/pictures/images');
+
+      // Verificar se a resposta foi bem-sucedida
+      if (!response.ok) {
+        console.error(`Erro na requisição: ${response.status} - ${response.statusText}`);
+        return;
+      }
+
+      // Tentar analisar o JSON apenas se a resposta não estiver vazia
+      const responseData = await response.json();
+
+      // Verificar se há dados válidos
+      if (!responseData || !responseData.images) {
+        console.error('Dados inválidos na resposta');
+        return;
+      }
+
       // Limpar a lista antes de adicionar novas imagens
       imageList.innerHTML = '';
-  
+
       // Adicionar cada imagem à lista
-      data.images.forEach(image => {
+      responseData.images.forEach(image => {
         const imgElement = document.createElement('img');
-        imgElement.src = `/uploads/${image.name}`;
+        imgElement.src = `http://localhost:3000/uploads/${image.name}`;
         imgElement.alt = image.comment || 'No comment';
-  
+
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete';
         deleteButton.className = 'btn btn-danger ms-2';
         deleteButton.addEventListener('click', () => deleteImage(image._id));
-  
+
         const container = document.createElement('div');
         container.className = 'mb-3';
         container.appendChild(imgElement);
         container.appendChild(deleteButton);
         imageList.appendChild(container);
       });
+    } catch (error) {
+      console.error('Erro ao carregar imagens:', error.message);
     }
-  
-    // Função para excluir uma imagem
-    async function deleteImage(id) {
-      const response = await fetch(`/pictures/images/${id}`, { method: 'DELETE' });
-      const data = await response.json();
-      
-      if (data.success) {
-        alert('Imagem excluída com sucesso!');
-        loadImages(); // Recarregar a lista após a exclusão
-      } else {
-        alert(`Erro: ${data.error}`);
-      }
-    }
-  
-    // Adicionar um ouvinte de evento para o formulário de upload
-    uploadForm.addEventListener('submit', async function (e) {
-      e.preventDefault();
-      
-      const formData = new FormData(this);
-      const response = await fetch('/pictures/upload', {
-        method: 'POST',
-        body: formData
+  }
+
+  // Função para excluir uma imagem
+  async function deleteImage(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pictures/images/${id}`, {
+        method: 'DELETE',
       });
-      
-      const data = await response.json();
-      
-      if (data.success) {
-        alert('Imagem enviada com sucesso!');
-        this.reset(); // Limpar o formulário após o upload
-        loadImages(); // Recarregar a lista após o upload
-      } else {
-        alert(`Erro: ${data.error}`);
+
+      // Verificar se a resposta foi bem-sucedida
+      if (!response.ok) {
+        const responseData = await response.json();
+        alert(`Erro: ${responseData.error}`);
+        return;
       }
-    });
-  
-    // Carregar as imagens ao carregar a página
-    loadImages();
-  });
-  
+
+      // Atualizar a lista de imagens após a exclusão
+      loadImages();
+    } catch (error) {
+      console.error('Erro ao excluir imagem:', error.message);
+    }
+  }
+
+  // Restante do seu código...
+
+  // Carregar as imagens ao carregar a página
+  loadImages();
+});
+
+
