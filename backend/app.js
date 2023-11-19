8b025fb3a3f1ebc6a0f2ecf5cc963b55a2f2fdbf
Germano <germanno.verde@gmail.com>
2023-11-19 16:26:19 -0300

    atualizando post no frontend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 /*
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -9,10 +8,10 @@ require('./db');
 
 const port = process.env.PORT || 3000;
 
-const pictureRouter = require('./routes/picture');
-
 app.use(cors()); // Middleware CORS
+app.use(express.json()); // Middleware para analisar o corpo das solicitações como JSON
 
+const pictureRouter = require('./routes/picture');
 app.use('/pictures', pictureRouter);
 
 app.listen(port, () => {
@@ -37,7 +36,4 @@ app.use('/pictures', pictureRouter);
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
-
-
-
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
   const uploadForm = document.getElementById('uploadForm');
   const imageList = document.getElementById('imageList');
 
@@ -75,4 +75,100 @@ document.addEventListener('DOMContentLoaded', () => {
   loadImages();
 });
 
+*/
 
+document.addEventListener('DOMContentLoaded', () => {
+  const uploadForm = document.getElementById('uploadForm');
+  const imageList = document.getElementById('imageList');
+
+  // Função para carregar imagens
+  async function loadImages() {
+    try {
+      const response = await fetch('http://localhost:3000/pictures/images');
+
+      // Verificar se a resposta foi bem-sucedida
+      if (!response.ok) {
+        console.error(`Erro na requisição: ${response.status} - ${response.statusText}`);
+        return;
+      }
+
+      // Tentar analisar o JSON apenas se a resposta não estiver vazia
+      const responseData = await response.json();
+
+      // Verificar se há dados válidos
+      if (!responseData || !responseData.images) {
+        console.error('Dados inválidos na resposta');
+        return;
+      }
+
+      // Limpar a lista antes de adicionar novas imagens
+      imageList.innerHTML = '';
+
+      // Adicionar cada imagem à lista
+      responseData.images.forEach(image => {
+        const imgElement = document.createElement('img');
+        imgElement.src = `http://localhost:3000/uploads/${image.name}`;
+        imgElement.alt = image.comment || 'No comment';
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'btn btn-danger ms-2';
+        deleteButton.addEventListener('click', () => deleteImage(image._id));
+
+        const container = document.createElement('div');
+        container.className = 'mb-3';
+        container.appendChild(imgElement);
+        container.appendChild(deleteButton);
+        imageList.appendChild(container);
+      });
+    } catch (error) {
+      console.error('Erro ao carregar imagens:', error.message);
+    }
+  }
+
+  // Função para excluir uma imagem
+  async function deleteImage(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pictures/images/${id}`, {
+        method: 'DELETE',
+      });
+
+      // Verificar se a resposta foi bem-sucedida
+      if (!response.ok) {
+        const responseData = await response.json();
+        alert(`Erro: ${responseData.error}`);
+        return;
+      }
+
+      // Atualizar a lista de imagens após a exclusão
+      loadImages();
+    } catch (error) {
+      console.error('Erro ao excluir imagem:', error.message);
+    }
+  }
+
+  // Event listener para o formulário de upload
+  uploadForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(uploadForm);
+    const response = await fetch('http://localhost:3000/pictures/upload', {
+      method: 'POST',
+      body: formData,
+    });
+
+    // Verificar se a resposta foi bem-sucedida
+    if (!response.ok) {
+      const responseData = await response.json();
+      alert(`Erro: ${responseData.error}`);
+      return;
+    }
+
+    // Limpar o formulário após o upload e recarregar a lista de imagens
+    uploadForm.reset();
+    loadImages();
+  });
+
+  // Carregar as imagens ao carregar a página
+  loadImages();
+});
