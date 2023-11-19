2817685c6447ec888a7561a7edc15e58f3fe639c
Germano <germanno.verde@gmail.com>
2023-11-19 18:46:31 -0300

    trabalho finalizado

--- backend/app.js
+++ backend/app.js
@@ -17,9 +17,12 @@ app.use('/pictures', pictureRouter);
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
+
 */
+
 const express = require('express');
 const cors = require('cors');
+const path = require('path');  // Importe o módulo 'path'.
 const app = express();
 
 require('dotenv').config();
@@ -27,8 +30,11 @@ require('./db');
 
 const port = process.env.PORT || 3000;
 
-app.use(cors()); // Middleware CORS
-app.use(express.json()); // Middleware para analisar o corpo das solicitações como JSON
+app.use(cors());
+app.use(express.json());
+
+// Configurar o middleware para servir arquivos estáticos na pasta de uploads.
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const pictureRouter = require('./routes/picture');
 app.use('/pictures', pictureRouter);
@@ -36,4 +42,3 @@ app.use('/pictures', pictureRouter);
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
-
--- frontend/script.js
+++ frontend/script.js
@@ -1,81 +1,5 @@
 
-/*document.addEventListener('DOMContentLoaded', () => {
-  const uploadForm = document.getElementById('uploadForm');
-  const imageList = document.getElementById('imageList');
 
-  // Função para carregar imagens
-  async function loadImages() {
-    try {
-      const response = await fetch('http://localhost:3000/pictures/images');
-
-      // Verificar se a resposta foi bem-sucedida
-      if (!response.ok) {
-        console.error(`Erro na requisição: ${response.status} - ${response.statusText}`);
-        return;
-      }
-
-      // Tentar analisar o JSON apenas se a resposta não estiver vazia
-      const responseData = await response.json();
-
-      // Verificar se há dados válidos
-      if (!responseData || !responseData.images) {
-        console.error('Dados inválidos na resposta');
-        return;
-      }
-
-      // Limpar a lista antes de adicionar novas imagens
-      imageList.innerHTML = '';
-
-      // Adicionar cada imagem à lista
-      responseData.images.forEach(image => {
-        const imgElement = document.createElement('img');
-        imgElement.src = `http://localhost:3000/uploads/${image.name}`;
-        imgElement.alt = image.comment || 'No comment';
-
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.className = 'btn btn-danger ms-2';
-        deleteButton.addEventListener('click', () => deleteImage(image._id));
-
-        const container = document.createElement('div');
-        container.className = 'mb-3';
-        container.appendChild(imgElement);
-        container.appendChild(deleteButton);
-        imageList.appendChild(container);
-      });
-    } catch (error) {
-      console.error('Erro ao carregar imagens:', error.message);
-    }
-  }
-
-  // Função para excluir uma imagem
-  async function deleteImage(id) {
-    try {
-      const response = await fetch(`http://localhost:3000/pictures/images/${id}`, {
-        method: 'DELETE',
-      });
-
-      // Verificar se a resposta foi bem-sucedida
-      if (!response.ok) {
-        const responseData = await response.json();
-        alert(`Erro: ${responseData.error}`);
-        return;
-      }
-
-      // Atualizar a lista de imagens após a exclusão
-      loadImages();
-    } catch (error) {
-      console.error('Erro ao excluir imagem:', error.message);
-    }
-  }
-
-  // Restante do seu código...
-
-  // Carregar as imagens ao carregar a página
-  loadImages();
-});
-
-*/
 
 document.addEventListener('DOMContentLoaded', () => {
   const uploadForm = document.getElementById('uploadForm');
@@ -115,9 +39,14 @@ document.addEventListener('DOMContentLoaded', () => {
         deleteButton.className = 'btn btn-danger ms-2';
         deleteButton.addEventListener('click', () => deleteImage(image._id));
 
+        const commentContainer = document.createElement('div');
+        commentContainer.textContent = `Comment: ${image.comment || 'No comment'}`;
+
         const container = document.createElement('div');
         container.className = 'mb-3';
         container.appendChild(imgElement);
+        container.appendChild(commentContainer); // Adiciona o container de comentários
+  
         container.appendChild(deleteButton);
         imageList.appendChild(container);
       });
